c64b9a4a0e6a13ea9e0f1a1479c6f003df90ca27
Conor Race <conorjat75@gmail.com>
2024-02-25 15:46:31 -0500

    Added .eslintrc file and allowed for lint fixes.

--- src/jsonResponses.js
+++ src/jsonResponses.js
@@ -1,5 +1,5 @@
 // Data Structure:
-// 
+//
 // {
 //  gallery: {
 //      name: string,
@@ -8,7 +8,7 @@
 //          image: {
 //              name: string,
 //              source: string,
-//              url: string    
+//              url: string
 //          }
 //      }
 //  }
@@ -17,126 +17,127 @@
 const galleries = {};
 
 const respondJSON = (request, response, status, object) => {
-    response.writeHead(status, { 'Content-Type': 'application/json' });
-    response.write(object);
-    response.end();
+  response.writeHead(status, { 'Content-Type': 'application/json' });
+  response.write(object);
+  response.end();
 };
-  
+
 const respondJSONMeta = (request, response, status) => {
-    response.writeHead(status, { 'Content-Type': 'application/json' });
-    response.end();
+  response.writeHead(status, { 'Content-Type': 'application/json' });
+  response.end();
 };
 
 const getGallery = (request, response) => {
-    // Set up response JSON.
-    const responseJSON = { galleries };
+  // Set up response JSON.
+  const responseJSON = { galleries };
 
-    // Respond with all galleries and response code 200.
-    return respondJSON(request, response, 200, JSON.stringify(responseJSON));
+  // Respond with all galleries and response code 200.
+  return respondJSON(request, response, 200, JSON.stringify(responseJSON));
 };
 
 const getGalleryMeta = (request, response, body) => {
-    // If no gallery name was provided OR if galleries does not contain gallery of name
-    // parameter, respond with a 400 error head/meta response.
-    if (!body.galName || !galleries[`gal-${body.galName}`]) {
-        return respondJSONMeta(request, response, 400);
-    }
+  // If no gallery name was provided OR if galleries does not contain gallery of name
+  // parameter, respond with a 400 error head/meta response.
+  if (!body.galName || !galleries[`gal-${body.galName}`]) {
+    return respondJSONMeta(request, response, 400);
+  }
 
-    // Else respond with response code of 200.
-    return respondJSONMeta(request, response, 200);
+  // Else respond with response code of 200.
+  return respondJSONMeta(request, response, 200);
 };
 
 const createGallery = (request, response, body) => {
-    // Set up response JSON.
-    const responseJSON = {
-        message: 'Name required to create gallery.'
+  // Set up response JSON.
+  const responseJSON = {
+    message: 'Name required to create gallery.',
+  };
+
+  // If no gallery name parameter, then respond with 400 error.
+  if (!body.galName) {
+    responseJSON.id = 'createGalleryMissingParam';
+    return respondJSON(request, response, 400, JSON.stringify(responseJSON));
+  }
+
+  let responseCode = 404;
+
+  // If gallery does not exist under name provided, create one and set up for 201 response.
+  if (!galleries[`gal-${body.galName}`]) {
+    responseCode = 201;
+    galleries[`gal-${body.galName}`] = {
+      name: body.galName,
+      created: Date.now.toString(),
+      images: {},
     };
 
-    // If no gallery name parameter, then respond with 400 error. 
-    if (!body.galName){
-        responseJSON.id = 'createGalleryMissingParam';
-        return respondJSON(request, response, 400, JSON.stringify(responseJSON));
-    }
-
-    let responseCode = 404;
-
-    // If gallery does not exist under name provided, create one and set up for 201 response.
-    if (!galleries[`gal-${body.galName}`]) {
-        responseCode = 201;
-        galleries[`gal-${body.galName}`] = {
-            name: body.galName,
-            created: Date.now.toString(),
-            images: {}
-        };
-
-        responseJSON.message = 'New gallery successfully created.';
+    responseJSON.message = 'New gallery successfully created.';
 
     // Else, if the gallery already, set up for 400 bad request.
-    } else {
-        responseCode = 400;
-        responseJSON.message = `Gallery under the name of '${body.galName}' already exists.`;
-        responseJSON.id = 'galleryAlreadyExists';
-    }
+  } else {
+    responseCode = 400;
+    responseJSON.message = `Gallery under the name of '${body.galName}' already exists.`;
+    responseJSON.id = 'galleryAlreadyExists';
+  }
 
-    // Return the response.
-    return respondJSON(request, response, responseCode, JSON.stringify(responseJSON));
+  // Return the response.
+  return respondJSON(request, response, responseCode, JSON.stringify(responseJSON));
 };
 
 const addImage = (request, response, body) => {
-    // Set up response JSON.
-    const responseJSON = {
-        message: 'Name and URL required to add image.'
-    };
+  // Set up response JSON.
+  const responseJSON = {
+    message: 'Name and URL required to add image.',
+  };
 
-    // If no image name or url parameters, then respond with 400 error.
-    if (!body.imgName || !body.imgURL) {
-        responseJSON.id = 'addImageMissingParams';
-        return respondJSON(request, response, 400, JSON.stringify(responseJSON));
-    }
+  // If no image name or url parameters, then respond with 400 error.
+  if (!body.imgName || !body.imgURL) {
+    responseJSON.id = 'addImageMissingParams';
+    return respondJSON(request, response, 400, JSON.stringify(responseJSON));
+  }
 
-    let responseCode = 204;
+  let responseCode = 204;
 
-    // If image does not exist under name provided, add it to selected gallery and set up for 201 response.
-    if (!galleries[`gal-${body.galName}`].images[`img-${body.imgName}`]) {
-        responseCode = 201;
-        galleries[`gal-${body.galName}`].images[`img-${body.imgName}`] = {};
+  // If image does not exist under name provided, add it to selected
+  // gallery and set up for 201 response.
+  if (!galleries[`gal-${body.galName}`].images[`img-${body.imgName}`]) {
+    responseCode = 201;
+    galleries[`gal-${body.galName}`].images[`img-${body.imgName}`] = {};
 
-        responseJSON.message = `New image successfully added to '${body.galName}'.`;
-    }
+    responseJSON.message = `New image successfully added to '${body.galName}'.`;
+  }
 
-    // Add (or update) all necessary data to new image object.
-    galleries[`gal-${body.galName}`].images[`img-${body.imgName}`].name = body.imgName;
-    galleries[`gal-${body.galName}`].images[`img-${body.imgName}`].url = body.imgURL;
-    if (body.imgSource) {
-        galleries[`gal-${body.galName}`].images[`img-${body.imgName}`].source = body.imgSource;
-    } else {
-        galleries[`gal-${body.galName}`].images[`img-${body.imgName}`].source = 'N/A';
-    }
+  // Add (or update) all necessary data to new image object.
+  galleries[`gal-${body.galName}`].images[`img-${body.imgName}`].name = body.imgName;
+  galleries[`gal-${body.galName}`].images[`img-${body.imgName}`].url = body.imgURL;
+  if (body.imgSource) {
+    galleries[`gal-${body.galName}`].images[`img-${body.imgName}`].source = body.imgSource;
+  } else {
+    galleries[`gal-${body.galName}`].images[`img-${body.imgName}`].source = 'N/A';
+  }
 
-    // If not a new image, respond with 204 meta response.
-    if (responseCode === 204) {
-        return respondJSONMeta(request, response, 204);
-    }
-    
-    // Else, respond with 201 response/
-    return respondJSON(request, response, 201, JSON.stringify(responseJSON));
+  // If not a new image, respond with 204 meta response.
+  if (responseCode === 204) {
+    return respondJSONMeta(request, response, 204);
+  }
+
+  // Else, respond with 201 response/
+  return respondJSON(request, response, 201, JSON.stringify(responseJSON));
 };
 
 const notFound = (request, response) => {
-    const responseJSON = { message: 'The page you were looking for could not be found.', id: 'contentNotFound' };
+  const responseJSON = { message: 'The page you were looking for could not be found.', id: 'contentNotFound' };
 
-    respondJSON(request, response, 404, JSON.stringify(responseJSON));
+  respondJSON(request, response, 404, JSON.stringify(responseJSON));
 };
 
 const notFoundMeta = (request, response) => {
-    respondJSONMeta(request, response, 404);
-}
+  respondJSONMeta(request, response, 404);
+};
 
 module.exports = {
-    getGallery,
-    getGalleryMeta,
-    createGallery,
-    addImage,
-    notFound,
-    notFoundMeta,
+  getGallery,
+  getGalleryMeta,
+  createGallery,
+  addImage,
+  notFound,
+  notFoundMeta,
 };
