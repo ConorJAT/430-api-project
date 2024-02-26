25f28ddc39f88c5ca3fe1ccaa4ccccc5e9a98fc9
Conor Race <conorjat75@gmail.com>
2024-02-25 20:21:08 -0500

    Reconfigured htmlResponses to be more efficient.

--- src/htmlResponses.js
+++ src/htmlResponses.js
@@ -1,26 +1,25 @@
 const fs = require('fs');
-const { request } = require('http');
 
 const index = fs.readFileSync(`${__dirname}/../hosted/client.html`);
 const css = fs.readFileSync(`${__dirname}/../hosted/styles.css`);
 const js = fs.readFileSync(`${__dirname}/../hosted/bundle.js`);
 
-const getIndex = (request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/html' });
-  response.write(index);
+const serveFile = (response, file, contentType) => {
+  response.writeHead(200, { contentType });
+  response.write(file);
   response.end();
+};
+
+const getIndex = (request, response) => {
+  serveFile(response, index, 'text/html');
 };
 
 const getCSS = (request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/css' });
-  response.write(css);
-  response.end();
+  serveFile(response, css, 'text/css');
 };
 
 const getJS = (request, response) => {
-  response.writeHead(200, { 'Content-Type': 'application/javascript' });
-  response.write(js);
-  response.end();
+  serveFile(response, js, 'application/javascript');
 };
 
 module.exports = {
